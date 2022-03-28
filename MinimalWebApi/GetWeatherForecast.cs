71820bdb0572a395d3cca1f8e2cd71ed38833b16
Sulibor Szlachetka <sulibor.szlachetka@cbre.com>
2022-03-28 22:37:35 +0200

    MinimalWebApi

---
+++ MinimalWebApi/GetWeatherForecast.cs
@@ -0,0 +1,31 @@
+﻿namespace MinimalWebApi;
+
+internal static class GetWeatherForecast
+{
+    private static readonly string[] Summaries =
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static void MapGetWeatherForecast(this WebApplication app)
+    {
+        app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateTime.Now.AddDays(index),
+                            Random.Shared.Next(-20, 55),
+                            Summaries[Random.Shared.Next(Summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+    }
+
+    private record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ MinimalWebApi/Program.cs
@@ -0,0 +1,20 @@
+using MinimalWebApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGetWeatherForecast();
+
+app.Run();
