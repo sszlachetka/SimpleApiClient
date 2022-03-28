4f3474b0510867cab47f112a2524c2dae806d479
Sulibor Szlachetka <sulibor.szlachetka@cbre.com>
2022-03-28 22:09:46 +0200

    Rename solution to SimpleApiClient

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using GitHubClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Extensions.Http;
+
+// https://docs.microsoft.com/en-us/dotnet/core/extensions/generic-host
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddLogging(logging => logging.AddSimpleConsole(options => options.SingleLine = true));
+        services.AddHostedService<Application>();
+        services.AddHttpClient<ApiClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://api.github.com");
+                client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ssz", "1.0"));
+            })
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(GetCircuitBreakerPolicy());
+    })
+    .Build();
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+}
+
+await host.RunAsync();
